79d78861eb78d5e26a98baad4961832e1cb79c91
Klaus Kämpf <kkaempf@suse.de>
2011-03-25 14:17:48 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backlog::Application.config.secret_token = '20c817b11817bc6bcbc73e1178bca78b538e7c098381dc31de8aea597842c780fab6e661ec941e0857e540dac860d52eca2cc87445804e343dc7c94f49997c25'
