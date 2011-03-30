c1c2c91af0b0fe366849029ab9b6270f49dffbcb
Klaus Kämpf <kkaempf@suse.de>
2011-03-30 13:46:46 +0200

    Backlog controller

---
+++ app/controllers/backlog_controller.rb
@@ -0,0 +1,11 @@
+class BacklogController < ApplicationController
+  def login
+  end
+
+  def logout
+  end
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Backlog::Application.routes.draw do
+  get "backlog/login"
+
+  get "backlog/logout"
+
+  get "backlog/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ Backlog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "backlog#index"
 
   # See how all your routes lay out with "rake routes"
 
