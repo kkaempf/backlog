d720bd643b2cdd3aff45c0ed51c9135ad499008f
Klaus Kämpf <kkaempf@suse.de>
2011-04-01 11:29:14 +0200

    Add name and email

--- app/models/backlog_rc.rb
+++ app/models/backlog_rc.rb
@@ -9,23 +9,26 @@ class BacklogRc
   extend ActiveModel::Naming
 
   validates :home, :presence => true
-  attr_reader :home
 
   def initialize
     begin
       File.open(File.expand_path(BACKLOGRC_PATH)) do |f|
 	rc = YAML.load(f)
-	@home = rc["home"]
+	self.home = rc["home"]
+	self.name = rc["name"]
+	self.email = rc["email"]
       end
     rescue
       Rails.logger.warn "~/.backlogrc not readable"
     end
-    @home = File.expand_path(@home || "~")
+    self.home = File.expand_path(@home || "~")
   end
 
   def home= homedir
     raise "homedir must be a directory" unless File.directory? homedir
     @home = homedir
   end
-
+  def home
+    @home
+  end
 end
