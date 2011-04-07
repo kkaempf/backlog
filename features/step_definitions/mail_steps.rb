4e5412c33689c7d5ff6eddb7ef5eeb3faf51ae7d
Klaus Kämpf <kkaempf@suse.de>
2011-04-07 10:46:58 +0200

    Ensure mail appears in backlog

--- features/step_definitions/mail_steps.rb
+++ features/step_definitions/mail_steps.rb
@@ -1,9 +1,12 @@
-Given /^I have a mail called "([^"]*)"$/ do |arg1|
-  samples_dir = File.expand_path(File.join(File.dirname(__FILE__),"..","samples"))
+def samples_dir
+  File.expand_path(File.join(File.dirname(__FILE__),"..","samples"))
+end
+
+Given /^I have a mail called "([^"]*)"$/ do |arg1| #"
   raise unless File.readable?(File.join(samples_dir, arg1))
 end
 
-Given /^I have mails with features$/ do  
+Given /^I have mails with features$/ do  #"
   $stderr.puts "Samples: #{samples_dir}"
   @mails = []
   Dir.open(samples_dir) do |d|
@@ -14,8 +17,7 @@ Given /^I have mails with features$/ do
   end
 end
   
-When /^I bounce "([^"]*)" to 'backlog'$/ do |arg1|
-  samples_dir = File.expand_path(File.join(File.dirname(__FILE__),"..","samples"))
+When /^I bounce "([^"]*)" to 'backlog'$/ do |arg1| #"
   $stderr.puts "Bounce #{arg1} to 'backlog'"
   File.open(File.join(samples_dir, arg1)) do |m|
     IO.popen(File.expand_path(File.join(File.dirname(__FILE__),"..","..","script","backlog")),"w") do |p|
@@ -24,6 +26,18 @@ When /^I bounce "([^"]*)" to 'backlog'$/ do |arg1|
   end
 end
     
-Then /^"([^"]*)" should appear in the backlog$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^"([^"]*)" should appear in the backlog$/ do |arg1| #"
+  subject = ""
+  File.open(File.join(samples_dir, arg1)) do |m|
+    while line = m.gets.chomp
+      next unless line =~ /^Subject: (.*)$/
+      subject = $1
+      break
+    end
+  end
+  if page.respond_to? :should
+    page.should have_content(subject)
+  else
+    assert page.has_content?(subject)
+  end
 end
